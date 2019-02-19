691f6430a78980a719e5c8673e09575499e96830
szdlt <40625240+szdlt@users.noreply.github.com>
2019-02-19 10:39:40 +0800

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -4,20 +4,20 @@
 
     //% color=50 weight=19
     //% icon="\uf1eb"
-declare namespace Mbit_IR {
+declare namespace Dlbot_IR {
 
     /**
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    //% block="on |%btn| button pressed" shim=Dlbot_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    //% block="connect ir receiver to %pin" shim=Dlbot_IR::init
     function init(pin: Pins): void;
 }
 
